a8648314d12bad632dfb2523fc5f461aa147f21e
Austin <lookcos@gmail.com>
2023-02-20 13:28:14 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 source /app/venv/black-box/bin/activate
-nohup /app/venv/black-box/uvicorn main:app --port 8000 --reload  > server.log 2>&1 &
+nohup /app/venv/black-box/bin/uvicorn main:app --port 8000 --reload  > server.log 2>&1 &
